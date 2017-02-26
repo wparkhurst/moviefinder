a80e7ce570dab23ab63c2ddd9eb9177315d1ebcf
Wayne Parkhurst <wsparkhu@asu.edu>
2017-02-26 14:07:15 -0700

    App Setup and Layout
    App Setup and Layout

---
+++ src/app/components/movies/movies.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'movies',
+  templateUrl: 'movies.component.html',
+})
+export class MoviesComponent  { }
